84595a14471fcc68619867f0463d4df06ab1e7d1
Liav Ben Shimon <liav1232111@gmail.com>
2024-07-29 08:51:40 +0300

    new

---
+++ 2024/July/25.07.2024/Task 7 condition/main.js
@@ -0,0 +1,12 @@
+// 1)
+let age = 20; // You can set this to any number to test
+let canVote;
+
+if (age >= 18) {
+    canVote = true;
+} else {
+    canVote = false;
+}
+
+console.log(canVote); // This will print true if age is 18 or greater, otherwise false
+//
---
+++ 2024/July/28.07.2024/main.js
@@ -0,0 +1,37 @@
+// console.log("test");
+
+// let i = 1;
+// while (i <= 100){
+//     console.log(i);
+//     i = i*2;
+// }
+// // //// first question do-while
+// let i = 1;
+// do {
+//     console.log(i++);
+// }
+// while (i <= 5);
+// // 5 question
+
+// let name
+// let listNames
+// do{(prompt.name
+
+// 1)
+let food =  ["apple" , "banana" , "meat" , "bluebery" , "egg"];
+//2)
+console.log(food[2]);
+//3)
+food [1] = "grape";
+console.log(food[1]);
+//4)
+food.push("chese");
+console.log(food);
+//5)
+food.shift (0);
+console.log(food);
+//6)
+for ( let i = 0 ; i>=0 ; i++){
+    i=food
+}
+console.log(f;
