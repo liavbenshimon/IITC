97bf73681dd2f0709e29caa38741d070a7ffb9d7
Liav Ben Shimon <liav1232111@gmail.com>
2024-07-30 12:57:07 +0300

    arrayhw

---
+++ 2024/July/28.07.2024/100+ array exercise/main.js
@@ -0,0 +1,41 @@
+//1) empty array called fruits
+let fruits = [];
+
+//2)
+let numbers = [ 1, 2 , 3 , 4, 5 ];
+
+//3)
+let colors = [ "red", "green", "blue"];
+
+//4)
+let mixed = [ 5 , "five" , "isItFive"];
+
+//5)
+let season = [ "Summer" , "Spring" , "Winter" , "Fall"];
+
+//--------------------------------------------------------------------------------------------
+//6
+console.log(numbers.length);
+
+//7)
+let emptyCheck = [];
+if (emptyCheck.length === 0) {
+    console.log("its empty");
+}else 
+console.log("the array isnt empty");
+
+//8)
+let dynamic = [1,2,3,4,5]
+console.log(dynamic.length);
+
+//9)
+dynamic.push(6);
+console.log(dynamic);
+
+//10)
+dynamic.pop(6);
+console.log(dynamic);
+
+////////////////////////////////////////////////////////////////////////
+
+//11)
