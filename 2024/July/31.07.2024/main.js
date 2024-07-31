2dc0af7ba97901814691e280f623c9d37d7e009e
Liav Ben Shimon <liav1232111@gmail.com>
2024-07-31 15:47:24 +0300

    class HW

--- 2024/July/31.07.2024/main.js
+++ 2024/July/31.07.2024/main.js
@@ -62,3 +62,46 @@
 // console.log(animal.species,animal.sound);
 // animal.isWild = "lolo";
 // console.log(animal.isWild);
+//Practice Time
+//1.1
+let car = {
+    make: "Mazda ",
+    model: 232,
+    year: 2002,
+    insurance: function () {
+                console.log(`${this.make} ${this.model} ${this.year}`);
+    }
+    };
+//1.2
+console.log(car.model);
+//1.3
+car.year = 2024;
+console.log(car[`year`]);
+//1.4
+car.color = `balck`;
+console.log(car.color);
+//1.5 -  I pushed function to the objec
+//1.6
+// שימוש במתודה מפתחות אובייקטים
+let pap = Object.keys(car);
+console.log(car);
+// For loop
+for (let i = 0; i < pap.length; i++) { 
+        console.log(`${pap[i]}: ${car[pap[i]]}`);
+}
+//1.7
+let person = {
+    name: "liav",
+    age: 27,
+    city: " NY"
+};
+//1.8
+let ad = Object.keys(person);
+
+for (i = 0; i<ad.length;i++){
+console.log(`${ad[i]}:${person[ad[i]]}`);
+};
+//
+
+
+
