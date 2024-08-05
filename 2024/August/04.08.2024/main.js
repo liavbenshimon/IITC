b441a7b765cb4d8d834f473241c06f45b577198c
Liav Ben Shimon <liav1232111@gmail.com>
2024-08-05 07:24:31 +0300

    a

---
+++ 2024/August/04.08.2024/main.js
@@ -0,0 +1,66 @@
+/*
+function creatNewPersons(names) {
+    let newPersons = [];
+for (let i = 0; i < names.length; i++) {
+  
+  let newPersons = {name:names[i]}
+    newPersons.push(names[i]);
+    
+}
+};
+
+    return newPersons = [];
+
+creatnewpersons(["yossi","liraz","baba"]);
+
+*/
+
+function groupBy(employees,department) {
+                 
+    let employees = [
+        { name: "John Doe", department: "Engineering", yearsOfExp: 5 },
+        { name: "Jane Smith", department: "Marketing", yearsOfExp: 8 },
+        { name: "Peter Johnson", department: "Engineering", yearsOfExp: 5 },
+        { name: "Lucy Brown", department: "Marketing", yearsOfExp: 10 },
+        { name: "Mike Davis", department: "Engineering", yearsOfExp: 3 },
+        { name: "Sara Wilson", department: "Marketing", yearsOfExp: 8 },
+      ];
+      console.log(groupBy(employees,"depaartment"));
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
