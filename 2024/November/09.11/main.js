a86f9a904f2ecd80a3ca109b78b61888292269b8
liav <liav1232111@gmail.com>
2024-11-11 10:39:19 +0200

    repeat

---
+++ 2024/November/09.11/main.js
@@ -0,0 +1,13 @@
+const add = (a,b) => a+b
+console.log(add(1,1));
+
+const subtraction = (a,b) => a-b
+console.log(subtraction(2,1));
+
+const multiplication = (a,b) => a*b
+console.log(multiplication(2,3));
+
+const division = (a,b) => a/b
+console.log(division(4,2));
+
+
